ac8340285c8728f19d85a86ef037ed61078e7d95
Evgeniy Hristoforu <evgenijhristo87@gmail.com>
2024-04-09 22:14:38 +0300

    Add openai_api module, update configure

---
+++ modules/openai_api.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+import gradio as gr
+import os
+import time
+import random
+
+from modules.load_configure import load_configure
+
+client = OpenAI(api_key=openai_key, base_url="https://api.chatanywhere.tech/v1")
+
+def chat_openai(message, history):
+    history_openai_format = []
+    for human, assistant in history:
+        history_openai_format.append({"role": "user", "content": human })
+        history_openai_format.append({"role": "assistant", "content":assistant})
+    history_openai_format.append({"role": "user", "content": message})
+  
+    response = client.chat.completions.create(model='gpt-3.5-turbo',
+    messages= history_openai_format,
+    temperature=1.0,
+    stream=True)
+
+    partial_message = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+              partial_message = partial_message + chunk.choices[0].delta.content
+              yield partial_message
